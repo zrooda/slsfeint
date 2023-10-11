f070d460672b38cb8c254bfb5d0ade7c6b35c7c5
Pure Žvýd™ <125412464+zrooda@users.noreply.github.com>
2023-10-11 16:27:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -15,11 +15,13 @@ const image = document.createElement("img");
 image.src = "https://assets.codepen.io/579289/stopwatch.png?format=auto";
 image.addEventListener("click", () => {dialog.close();});
 dialog.appendChild(image);
+body.appendChild(dialog);
 
 // Styling
 const styles = document.createElement("style");
-style.textContent = `
+styles.textContent = `
 .sls-button {position: fixed; top: 10px; right: 10px;}
 .sls-dialog {display: flex; place-content: center center; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,0.2);}
 .sls-dialog img {max-width: 100%; max-height: 100%}
 `;
+body.appendChild(styles);
