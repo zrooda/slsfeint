a67b08ada7a09f611eaba0c4c96d08412611415d
Pure Žvýd™ <125412464+zrooda@users.noreply.github.com>
2023-10-11 16:46:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,24 +3,27 @@ const body = document.body;
 
 const dialog = document.createElement("dialog");
 dialog.classList.add("sls-dialog");
-
+const container = document.createElement("div");
+container.classList.add("sls-container");
+dialog.appendChild(container);
 const image = document.createElement("img");
 image.src = "https://assets.codepen.io/579289/stopwatch.png?format=auto";
-image.addEventListener("click", () => {dialog.close();});
+image.addEventListener("click", () => {dialog.removeAttribute("open");});
 dialog.appendChild(image);
 body.appendChild(dialog);
 
 const button = document.createElement("button");
 button.textContent = "Help!";
 button.classList.add("sls-button");
-button.addEventListener("click", () => {dialog.showModal();});
+button.addEventListener("click", () => {dialog.setAttribute("open", true);});
 body.appendChild(button);
 
 // Styling
 const styles = document.createElement("style");
 styles.textContent = `
 .sls-button {position: fixed; top: 10px; right: 10px;}
-.sls-dialog {display: flex; place-content: center center; position: fixed; inset: 0; background-color: rgba(0,0,0,0.2);}
+.sls-dialog {position: fixed; inset: 0; background-color: rgba(0,0,0,0.2);}
+.sls-container {width: 100%; height: 100%; display: flex; place-content: center center;}
 .sls-dialog img {max-width: 100%; max-height: 100%}
 `;
 body.appendChild(styles);
