2de792aca014b573c7980d8f76fcca3a3f6313e9
Pure Žvýd™ <125412464+zrooda@users.noreply.github.com>
2023-10-11 17:13:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -25,6 +25,6 @@ html, body {width: 100%; height: 100%;}
 .sls-button {position: fixed; top: 10px; right: 10px;}
 .sls-dialog {box-sizing: border-box; width: 100%; height: 100%; position: fixed; left: 0; top: 0; background-color: rgba(0,0,0,0.2); border: 0; z-index: 1000;}
 .sls-container {width: 100%; height: 100%; display: flex; place-content: center center;}
-.sls-dialog img {max-width: 100%; max-height: 100%}
+.sls-dialog img {max-width: 100%; max-height: 100%; object-fit: contain;}
 `;
 body.appendChild(styles);
