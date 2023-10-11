0cf6b86d11446b577eb63b58df4ff3d5e4b533e9
Pure Žvýd™ <125412464+zrooda@users.noreply.github.com>
2023-10-11 16:25:16 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+// Refs
+const body = document.body;
+
+const button = document.createElement("button");
+button.textContent = "Help!";
+button.classList.add("sls-button");
+button.addEventListener
+body.appendChild(button);
+
+const dialog = document.createElement("dialog");
+dialog.classList.add("sls-dialog");
+body.appendChild(dialog);
+
+const image = document.createElement("img");
+image.addEventListener("click", () => {dialog.close();});
+dialog.appendChild(image);
+
+// Styling
+const styles = document.createElement("style");
+style.textContent = `
+.sls-button {position: fixed; top: 10px; right: 10px;}
+.sls-dialog {display: flex; place-content: center center; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,0.2);}
+.sls-dialog img {max-width: 100%; max-height: 100%}
+`;
