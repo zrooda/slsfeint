2523d0ea620975ca6ad2c9e83c32733632886ff1
Pure Žvýd™ <125412464+zrooda@users.noreply.github.com>
2023-10-23 12:12:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -29,11 +29,12 @@ styles.textContent = `
 html, body {width: 100%; height: 100%;}
 body {margin: 0; font-family: "Playpen Sans", cursive;}
 *, *::before, *::after {box-sizing: border-box;}
-.sls-button {position: fixed; top: 10px; right: 10px;}
+.sls-button {position: fixed !important; top: 10px; right: 10px;}
 .sls-dialog {width: 100%; height: 100%; position: fixed; left: 0; top: 0; background-color: rgba(0,0,0,0.4); border: 0; z-index: 1000;}
 .sls-container {width: 100%; height: 100%; display: flex; place-content: center center;}
 .sls-dialog img {max-width: 100%; max-height: 100%; object-fit: contain; border-radius: 12px;}
 .pushable {
+  position: relative;
   border: none;
   background: transparent;
   padding: 0;
